56c2ed1efab36013965c3d61869bb671dcf29f02
Jouni Alanen <jouni@diory.me>
2018-07-25 05:16:10 +0300

    Sketched out first spec for ReceiptForm

---
+++ spec/receipt-form-spec.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { configure, shallow } from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16';
+import { ReceiptForm } from '../app/forms'
+import { DiographStore } from 'diograph-store'
+
+declare var Promise: any;
+
+describe('<ReceiptForm />', () => {
+  let wrapper, component, secrets
+  configure({ adapter: new Adapter() })
+
+  it('renders diory name when diory found', () => {
+    let getDiorySpy = spyOn(DiographStore, "getDiory").and.returnValue({name: "Found diory"})
+
+    wrapper = shallow(
+      <ReceiptForm
+        modelId="123-abc"
+        editMode="false" />
+    )
+    component = wrapper.instance()
+
+    expect(getDiorySpy.calls.argsFor(0)).toEqual(["123-abc"])
+    let componentText = wrapper.find('#name').text();
+    expect(componentText).toEqual("Found diory")
+  })
+
+  it('calls createDiory if diory not found', () => {
+    spyOn(DiographStore, "getDiory").and.returnValue(null)
+    let createDiorySpy = spyOn(DiographStore, "createDiory")
+    expect(createDiorySpy.calls.argsFor(0)).toEqual([{name: "New receipt"}])
+  })
+
+})
