59574709cfdb6364d8f0345b0b4988787d6ee349
Jouni Alanen <jouni@diory.me>
2018-07-28 08:19:10 +0300

    TODO comment added

--- app/diograph-receipt-app.tsx
+++ app/diograph-receipt-app.tsx
@@ -17,12 +17,14 @@ export class DiographReceiptApp extends React.Component<ReceiptAppProps, Receipt
   }
 
   render() {
+    // TODO: We shouldn't need both: /:id & /:id/:edit
+    // => don't know how to get /:id/:edit to match also with /receipt/123-abc
     return (
       <div>
         <Switch>
           <Route path='/receipt/:id/:edit' component={ReceiptForm} />
           <Route path='/receipt/:id' component={ReceiptForm} />
-          <Route path='/place/:id/edit' component={PlaceForm}/>
+          <Route path='/place/:id/:edit' component={PlaceForm}/>
           <Route path='/place/:id' component={PlaceForm}/>
           <Route path='/product/:id/:edit' component={ProductForm}/>
           <Route path='/product/:id' component={ProductForm}/>
