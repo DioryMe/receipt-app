035e7e5353870e07328a26968e544573140e4ad4
Jouni Alanen <jouni@diory.me>
2018-07-28 14:44:01 +0300

    Edit mode ON & OFF specs + call getDiory spec implemented

--- spec/receipt-form-spec.tsx
+++ spec/receipt-form-spec.tsx
@@ -1,33 +1,58 @@
 import * as React from 'react'
-import { configure, shallow } from 'enzyme'
+import { configure, mount } from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16';
 import { ReceiptForm } from '../app/receipt-form'
 import { DiographStore } from 'diograph-store'
+import { MemoryRouter, Route } from 'react-router';
 
 declare var Promise: any;
+
 
 describe('<ReceiptForm />', () => {
   let wrapper, component, secrets
   configure({ adapter: new Adapter() })
 
-  // This should fail as no mention of DiographStore.getDiory() yet anywhere...
-  // => but shouldn't fail for this.props.match being undefined... ("Cannot read property 'params' of undefined")
+  beforeEach(() => {
+    this.jsdom = require('jsdom-global')()
+  })
+
   it('calls DiographStore.getDiory when modelId is given', () => {
     let getDiorySpy = spyOn(DiographStore, "getDiory").and.returnValue({name: "Found diory"})
 
-    // TODO: Would <ReceiptForm /> be enough?
-    // - modelId comes from the url
-    // - editMode comes from the url
-    wrapper = shallow(
-      <ReceiptForm
-        modelId="123-abc"
-        editMode="false" />
+    wrapper = mount(
+      <MemoryRouter initialEntries={[ '/receipt/123-abc' ]}>
+        <Route path='/receipt/:id' component={ReceiptForm} />
+      </MemoryRouter>
     )
     component = wrapper.instance()
 
     expect(getDiorySpy.calls.argsFor(0)).toEqual(["123-abc"])
     let componentText = wrapper.find('#name').text();
     expect(componentText).toEqual("Found diory")
+  })
+
+  it('has edit mode ON when /receipt/123-abc/edit', () => {
+    wrapper = mount(
+      <MemoryRouter initialEntries={[ '/receipt/123-abc' ]}>
+        <Route path='/receipt/:id' component={ReceiptForm} />
+      </MemoryRouter>
+    )
+    component = wrapper.instance()
+
+    let componentText = wrapper.find('div').first().text();
+    expect(componentText).toContain("Edit mode ON")
+  })
+
+  it('has edit mode OFF when /receipt/123-abc', () => {
+    wrapper = mount(
+      <MemoryRouter initialEntries={[ '/receipt/123-abc/edit' ]}>
+        <Route path='/receipt/:id/:edit' component={ReceiptForm} />
+      </MemoryRouter>
+    )
+    component = wrapper.instance()
+
+    let componentText = wrapper.find('div').first().text();
+    expect(componentText).toContain("Edit mode OFF")
   })
 
   /*
